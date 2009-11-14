69ceeec642b63beb41f4d51f5620daea79d54a9d
klickverbot <klickverbot@gmail.com>
2009-11-14 05:02:08 +0100

    Write real D code in »simple« example.

--- Examples/d/simple/runme.d
+++ Examples/d/simple/runme.d
@@ -3,23 +3,25 @@ module runme;
 static import example;
 import tango.io.Stdout;
 
-/*
- * Call our gcd() function.
- */
-int x = 42;
-int y = 105;
-int g = example.gcd( x, y );
-Stdout.format( "The gcd of {} and {} is {}.", x, y, g ).newline;
+void main() {
+  /*
+   * Call our gcd() function.
+   */
+  int x = 42;
+  int y = 105;
+  int g = example.gcd( x, y );
+  Stdout.format( "The gcd of {} and {} is {}.", x, y, g ).newline;
 
-/*
- * Manipulate the Foo global variable.
- */
+  /*
+   * Manipulate the Foo global variable.
+   */
 
-// Output its current value
-Stdout.format( "Foo = {}" + example.Foo ).newline;
+  // Output its current value
+  Stdout.format( "Foo = {}", example.Foo ).newline;
 
-// Change its value
-example.Foo = 3.1415926;
+  // Change its value
+  example.Foo = 3.1415926;
 
-// See if the change took effect
-Stdout.format( "Foo = {}" + example.Foo ).newline;
+  // See if the change took effect
+  Stdout.format( "Foo = {}", example.Foo ).newline;
+}
