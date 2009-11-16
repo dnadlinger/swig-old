87450bfdf0bd06de0b3cc3b6e6289ca134c6febc
klickverbot <klickverbot@gmail.com>
2009-11-16 22:40:12 +0100

    Ported »funcptr« example – working.

---
+++ Examples/d/funcptr/runme.d
@@ -0,0 +1,21 @@
+static import example;
+import tango.io.Stdout;
+
+void main() {
+  int a = 37;
+  int b = 42;
+
+  // Now call our C function with a bunch of callbacks
+
+  Stdout( "Trying some C callback functions:" ).newline;
+  Stdout( "    a        = " )( a ).newline;
+  Stdout( "    b        = " )( b ).newline;
+  Stdout( "    ADD(a,b) = " )( example.do_op(a,b,example.ADD) ).newline;
+  Stdout( "    SUB(a,b) = " )( example.do_op(a,b,example.SUB) ).newline;
+  Stdout( "    MUL(a,b) = " )( example.do_op(a,b,example.MUL) ).newline;
+
+  Stdout( "\nThe names of the C callback functino classes in D:" ).newline;
+  Stdout( "    ADD      = " )( example.ADD.classinfo.name ).newline;
+  Stdout( "    SUB      = " )( example.SUB.classinfo.name ).newline;
+  Stdout( "    MUL      = " )( example.MUL.classinfo.name ).newline;
+}
