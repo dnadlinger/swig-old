ab21b34e8b2ece556217bf0c4d74e5aed38b5f69
klickverbot <klickverbot@gmail.com>
2009-12-08 15:43:31 +0100

    Added runtime test to »varargs«.

---
+++ Examples/test-suite/d/varargs_runme.d
@@ -0,0 +1,19 @@
+module varargs_runme;
+
+import varargs;
+
+void main() {
+  if (test("Hello") != "Hello") {
+    throw new Exception("Test 1 failed");
+  }
+
+  auto f = new Foo("Greetings");
+
+  if (f.str != "Greetings") {
+    throw new Exception("Test 2 failed");
+  }
+
+  if (f.test("Hello") != "Hello") {
+    throw new Exception("Test 3 failed");
+  }
+}
