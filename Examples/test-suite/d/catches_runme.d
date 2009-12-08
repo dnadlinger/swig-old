48b360981c298a152f17aba05f6adabd1f2c52df
klickverbot <klickverbot@gmail.com>
2009-12-08 16:16:56 +0100

    Cleaned up »catches« runtime test.

--- Examples/test-suite/d/catches_runme.d
+++ Examples/test-suite/d/catches_runme.d
@@ -3,62 +3,31 @@ module catches_runme;
 import catches;
 
 void main() {
-  // test_catches()
+  test({ test_catches(1); }, "C++ int exception thrown, value: 1");
+  test({ test_catches(2); }, "two");
+  test({ test_catches(3); }, "C++ ThreeException const & exception thrown");
+
+  test({ test_exception_specification(1); }, "C++ int exception thrown, value: 1");
+  test({ test_exception_specification(2); }, "unknown exception");
+  test({ test_exception_specification(3); }, "unknown exception");
+
+  test({ test_catches_all(1); }, "unknown exception");
+}
+
+void test(void delegate() command, char[] expectedMessage) {
+  bool didntThrow;
   try {
-    catches.test_catches(1);
-    throw new Exception("missed exception");
+    command();
+    didntThrow = true;
   } catch (Exception e) {
-    if (e.msg != "C++ int exception thrown, value: 1")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
+    if (e.msg != expectedMessage) {
+      throw new Exception("Failed to propagate C++ exception. Expected '" ~
+	expectedMessage ~ "', but received '" ~ e.msg ~ "'.");
+    }
   }
 
-  try {
-    catches.test_catches(2);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "two")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
-  }
-
-  try {
-    catches.test_catches(3);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "C++ ThreeException const & exception thrown")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
-  }
-
-  // test_exception_specification()
-  try {
-    catches.test_exception_specification(1);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "C++ int exception thrown, value: 1")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
-  }
-
-  try {
-    catches.test_exception_specification(2);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "unknown exception")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
-  }
-
-  try {
-    catches.test_exception_specification(3);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "unknown exception")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
-  }
-
-  // test_catches_all()
-  try {
-    catches.test_catches_all(1);
-    throw new Exception("missed exception");
-  } catch (Exception e) {
-    if (e.msg != "unknown exception")
-      throw new Exception("Failed to propagate C++ exception. Received instead: " ~ e.msg);
+  if (didntThrow) {
+    throw new Exception("Failed to propagate C++ exception. Expected '" ~
+	expectedMessage ~ "', but no exception was thrown.");
   }
 }
