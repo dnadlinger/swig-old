4ad1e7e9d4a9509f20f36fff0382897220be31b1
klickverbot <klickverbot@gmail.com>
2009-12-08 16:18:24 +0100

    Cleaned up »default_constructor« runtime test.

--- Examples/test-suite/d/default_constructor_runme.d
+++ Examples/test-suite/d/default_constructor_runme.d
@@ -1,15 +1,14 @@
 module default_constructor_runme;
 
-import tango.core.Exception;
 import default_constructor;
 
 void main() {
   // Protected destructor test.
   try {
-      {
-	scope g = new G();
-      }
-      throw new Exception("Protected destructor exception should have been thrown");
+    {
+      scope g = new G();
+    }
+    throw new Exception("Protected destructor exception should have been thrown");
   } catch (Exception e) {
     if (e.msg != "C++ destructor does not have public access") {
       throw e;
@@ -18,10 +17,10 @@ void main() {
 
   // Private destructor test.
   try {
-      {
-	scope f = new FFF();
-      }
-      throw new Exception("Private destructor exception should have been thrown");
+    {
+      scope f = new FFF();
+    }
+    throw new Exception("Private destructor exception should have been thrown");
   } catch (Exception e) {
     if (e.msg != "C++ destructor does not have public access") {
       throw e;
