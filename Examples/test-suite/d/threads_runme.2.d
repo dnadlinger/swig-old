4f1b58b9379eef68260880c6217881149b5f40af
David Nadlinger <code@klickverbot.at>
2010-09-15 01:05:57 +0200

    Ported »threads« runtime test to D2.

---
+++ Examples/test-suite/d/threads_runme.2.d
@@ -0,0 +1,67 @@
+module threads_runme;
+
+import core.thread;
+import std.conv;
+import std.exception;
+import std.range;
+import std.stdio;
+import threads.Kerfuffle;
+
+// Spawn 8 threads.
+enum THREADS = iota(0, 9);
+
+// Run test for a few seconds on a 1GHz machine.
+enum WORK_RANGE = iota(0, 30000);
+
+void main() {
+  auto kerf = new Kerfuffle();
+  TestThread[] threads;
+
+  // Invoke the threads.
+  foreach (i; THREADS) {
+    auto thread = new TestThread(kerf);
+    threads ~= thread;
+    thread.name = to!string(i);
+    thread.start();
+  }
+
+  // Wait for the threads to finish.
+  foreach (thread; threads) {
+    thread.join();
+  }
+
+  // Check if any thread has failed.
+  foreach (thread; threads) {
+    enforce(!thread.failed);
+  }
+}
+
+class TestThread : Thread {
+  this(Kerfuffle kerf) {
+    super(&run);
+    m_kerf = kerf;
+  }
+
+  void run() {
+    failed = false;
+    try {
+      foreach (i; WORK_RANGE) {
+	string given = "This is the test string that should come back. A number: " ~ to!string(i);
+	string received = m_kerf.StdString(given);
+	enforce(received == given, "StdString string does not match. Received: '" ~ received ~ "'. Expected: '" ~ given ~ "'.");
+      }
+      foreach (i; WORK_RANGE) {
+	string given = "This is the test string that should come back. A number: " ~ to!string(i);
+	string received = m_kerf.CharString(given);
+	enforce(received == given, "CharString string does not match. Received: '" ~ received ~ "'. Expected: '" ~ given ~ "'.");
+      }
+    } catch (Exception e) {
+      writefln("Test failed (thread %s): %s", name, e.msg);
+      failed = true;
+    }
+  }
+
+  bool failed;
+private:
+  Kerfuffle m_kerf;
+}
